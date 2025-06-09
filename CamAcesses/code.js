3c11522ad71268bd9979459f92dc92b5a335ab0f
jogotcode <106411062+jogotcode@users.noreply.github.com>
2025-06-09 16:24:46 -0400

    Delete CamAcesses directory

--- CamAcesses/code.js
+++
@@ -1,7 +0,0 @@
-
-const video = document.getElementById('Cam-Video');
-
-navigator.mediaDevices.getUserMedia({video: true})
-  .then(stream =>{Video.srcObject = stream;})
-  .catch(error => {console.error('Camera Access Error',error);
-  });
